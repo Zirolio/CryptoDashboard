fdbc4430255b3addf4f699917a644c018f4c9be8
Zirolio <25364Zirolio@gmail.com>
2026-04-08 15:00:32 +0300

    Fix base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    tsconfigPaths: true
-  }
+    tsconfigPaths: true,
+  },
+  base: '/CryptoDashboard/'
 })
